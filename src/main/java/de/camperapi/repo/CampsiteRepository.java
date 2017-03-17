7f6f4ccc189eb9e7315ba7dcd90ae6e66c064902
dome4 <dominic.seitz@gmx.de>
2017-03-17 16:48:00 +0100

    interface changed to PagingAndSortingRepository

--- src/main/java/de/camperapi/repo/CampsiteRepository.java
+++ src/main/java/de/camperapi/repo/CampsiteRepository.java
@@ -1,14 +1,14 @@
 package de.camperapi.repo;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import de.camperapi.entity.Campsite;
 
 @RepositoryRestResource(collectionResourceRel = "campsites", path = "campsites")
-public interface CampsiteRepository extends CrudRepository<Campsite, Integer> {
+public interface CampsiteRepository extends PagingAndSortingRepository<Campsite, Integer> {
 
 	
 	/**
